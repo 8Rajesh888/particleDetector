6eb10cc380972ff8ee4d29745f98da0528334b64
Rajesh <a9rajesh.c@gmail.com>
2026-09-25 23:40:09 +0530

    Step 1 Completed

--- sketch.js
+++ sketch.js
@@ -1,35 +1,57 @@
 const r = require("raylib");
 
-const screenWidth = 400;
-const screenHeight = 400;
-const FPS = 20;
+const screenWidth = 1700;
+const screenHeight = 900;
+const rectWidth = 20;
 
-let rectWidth = 200;
-let rectHeight = 200;
+let rectangleXAxis = 0;
+let isMovingForward = true;
 
 function running() {
     return !r.WindowShouldClose();
 }
 
 function setup() {
-    r.InitWindow(screenWidth, screenHeight, "Rectangle");
+    const FPS = 50;
+
+    r.InitWindow(screenWidth, screenHeight, "PARTICLE DETECTOR");
     r.SetTargetFPS(FPS);
 }
 
-function update() {
 
+function update() {
+    rectangleXAxis = detectMove();
+
+    if (isCollidingWall()) {
+        isMovingForward = !isMovingForward;
+    }
+}
+
+function isCollidingWall() {
+    return isCollidingrightWall() || isCollidingLeftWall();
+}
+
+function isCollidingLeftWall() {
+    return rectangleXAxis <= 0 && !isMovingForward;
+}
+
+function isCollidingrightWall() {
+    return rectangleXAxis + rectWidth >= screenWidth;
+}
+
+function detectMove() {
+    const detectorSpeed = 3;
+
+    return ((rectangleXAxis < screenWidth) && isMovingForward) ? rectangleXAxis + detectorSpeed : rectangleXAxis - detectorSpeed;
 }
 
 function draw() {
-
     r.BeginDrawing();
     r.ClearBackground(r.BLACK);
 
-
+    r.DrawRectangle(rectangleXAxis, 0, rectWidth, screenHeight, r.WHITE);
 
     r.EndDrawing();
-
-
 }
 
 function teardown() {
